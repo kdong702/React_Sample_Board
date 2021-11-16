185f519b987ed529d819f40b2b5b15dbcd9ca676
김동현 <kdong702@naver.com>
2021-11-16 09:46:53 +0900

    checkbox

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {changeSearchType,changeSearchKeyword,changeInput} from '../action/search';
 
 const Search = ()=>{
     const searchType = useSelector(state => state.search.searchType);
-    const searchKeyword = useSelector(state => state.search.searchKeyword);
     const dispatch = useDispatch();
-   
+    
+    // s search2
+    const [type2, setType2] = useState('title');
+    const [keyword2, setKeyword2] = useState('');
+    
+    var keyword = keyword2;
+    var type = type2;
 
-        
+    const searchHandler = (e) =>{
+        console.log("search!!" + type + keyword);
+        dispatch(changeSearchType(type));
+        dispatch(changeSearchKeyword(keyword));
+    }
+    //selectList 선택시
+    const selectHandler = (e) => {
+        type=e.target.value;
+        console.log(type);
+        setType2(type);
+    }
+
+    // e search2
+    //검색어 바뀔시
+    const changeHandler = (e) => {
+        dispatch(changeInput(e.target.value));
+        keyword = e.target.value; //search2
+        console.log(keyword); //search2
+        setKeyword2(keyword); //search2
+    }
+    
     const submitHandler = (e) =>{
         e.preventDefault();
         dispatch(changeSearchType(e.target.selectList.value));
         dispatch(changeSearchKeyword(e.target.searchKeyword.value));
     }
 
-    const changeHandler = (e) => {
-        dispatch(changeInput(e.target.value));
-    }
+    
+
+    
           
     var selected = searchType == null ? "" : searchType;
 
@@ -35,7 +60,7 @@ const Search = ()=>{
                                 <tbody>
                                     <tr>
                                         <th>
-                                            <select name="selectList" id="selectList" className="ui_sel" style={{width: "100px"}} defaultValue={selected} >
+                                            <select name="selectList" id="selectList" className="ui_sel" style={{width: "100px"}} defaultValue={selected} onChange={selectHandler} >
                                                     <option value="title" >제목</option>
                                                     <option value="contents" >내용</option>
                                                     <option value="all">제목+내용</option>
@@ -52,10 +77,10 @@ const Search = ()=>{
                 </div>
                 
                 <div className="colgroup btn" style={{width: "12%"}}>
-                    {/* <span className="bar">|</span>
-                    <a href="#" className="btn_black" onClick={searchHandler}><span>검색</span></a> 
-                    <span className="barr">|</span> */}
-                     <button type="submit" className="btn_black" style={{color:"white"}}>검색</button> 
+                     <span className="bar">|</span>
+                    <a  className="btn_black" onClick={searchHandler} style={{cursor:"pointer"}}><span>검색2</span></a> 
+                    <span className="barr">|</span> 
+                      <button type="submit" className="btn_black" style={{color:"white"}}>검색</button> 
                 </div>
                 
             </form>
--- src/reducer/list.js
+++ src/reducer/list.js
@@ -1,15 +1,28 @@
 import {  handleActions } from "redux-actions";
-import {CHANGE_TOTALCOUNT} from '../action/list'
+import {CHANGE_TOTALCOUNT,CREATE_CHECKBOX,DELETE_CHECKBOX} from '../action/list'
+
+
 
 const initialState = {
     totalCount: 0,
+    checkedList : [
+       
+    ],
 };
 
 export default handleActions(
     {
         [CHANGE_TOTALCOUNT]:(state,action) => ({
             ...state,
-            totalCount: action.payload
+            totalCount: action.payload,
+        }),
+        [CREATE_CHECKBOX]:(state,action) => ({
+            ...state,
+            checkedList:state.checkedList.concat(action.payload),
+        }),
+        [DELETE_CHECKBOX]: (state, action) => ({
+            ...state,
+            checkedList: state.checkedList.filter(item => item !== action.payload),
         }),
     },
     initialState
