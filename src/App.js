f3399005a885bfabc5f9a81fa92a1098497fab02
jeongmiAhn <me_meome@naver.com>
2021-11-17 13:59:32 +0900

    [211117] 삭제 수정 병합

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React  from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BoardList from './components/BoardList';
 import BoardNew from './components/BoardNew';
 import BoardDetail from './components/BoardDetail';
