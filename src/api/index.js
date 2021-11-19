5d3a57330a9223b40b9346293d6ed7350a99d2b5
jeongmiAhn <me_meome@naver.com>
2021-11-19 15:41:09 +0900

    [211119] api 파일 분리

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+const listApi = 'http://192.168.100.74:18080/homepage/api/notification/list.do';
+const detailApi = 'http://192.168.100.74:18080/homepage/api/notification/detail.do';
+const fileListApi = 'http://192.168.100.74:18080/homepage/api/notification/fileList.do';
+const downloadApi = 'http://192.168.100.74:18080/homepage/api/notification/download.do';
+const insertApi = 'http://192.168.100.74:18080/homepage/api/notification/insert.do';
+const updateApi = 'http://192.168.100.74:18080/homepage/api/notification/update.do';
+const deleteApi = 'http://192.168.100.74:18080/homepage/api/notification/delete.do';
+
+export {listApi, detailApi, fileListApi, downloadApi, insertApi, updateApi, deleteApi};
